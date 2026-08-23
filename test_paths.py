d7737288543816265d75751573bb3e7887fe5ae3
Sergey Alexeev <sergeyalexeev@yahoo.com>
2026-08-23 12:47:13 +0300

    Release v0.2.9

--- test_paths.py
+++ test_paths.py
@@ -634,7 +634,7 @@ class DependencyRootTest(unittest.TestCase):
             ):
                 build_name = (
                     "${SERVICEGEN_CPPBOOST_BUILD_VOLUME:-"
-                    "cppboostexample_cpp-cmake-build-v0.2.8}"
+                    "cppboostexample_cpp-cmake-build-v0.2.9}"
                 )
                 (root / name).write_text(
                     "volumes:\n"
