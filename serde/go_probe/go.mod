51ed24c6c07a72a30067985439b52b6656c10f82
Sergey Alexeev <sergeyalexeev@yahoo.com>
2026-08-26 21:36:47 +0300

    Prepare dependencies for v0.2.13

--- serde/go_probe/go.mod
+++ serde/go_probe/go.mod
@@ -2,6 +2,6 @@ module servicelib-conformance-serde-probe
 
 go 1.25.4
 
-require github.com/gorundebug/servicelib v0.2.12
+require github.com/gorundebug/servicelib v0.2.13
 
 replace github.com/gorundebug/servicelib => ../../../servicelib
--- test_paths.py
+++ test_paths.py
@@ -821,7 +821,7 @@ class DependencyRootTest(unittest.TestCase):
             ):
                 build_name = (
                     "${SERVICEGEN_CPPBOOST_BUILD_VOLUME:-"
-                    "cppboostexample_cpp-cmake-build-v0.2.12}"
+                    "cppboostexample_cpp-cmake-build-v0.2.13}"
                 )
                 (root / name).write_text(
                     "volumes:\n"
