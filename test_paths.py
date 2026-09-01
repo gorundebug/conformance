8d5d54503a37abb89153a2f067c16d070c26bf9b
Sergey Alexeev <sergeyalexeev@yahoo.com>
2026-09-01 11:43:41 +0300

    Verify native npm binary proxy key

--- test_paths.py
+++ test_paths.py
@@ -43,7 +43,7 @@ class DependencyRootTest(unittest.TestCase):
                 dependency_download_mirrors,
                 "environment",
                 return_value={
-                    "npm_config_confluent_kafka_javascript_binary_host_mirror": mirror
+                    "npm_config_confluent_kafka-javascript_binary_host_mirror": mirror
                 },
             ),
         ):
@@ -54,7 +54,7 @@ class DependencyRootTest(unittest.TestCase):
         )
         self.assertEqual(
             environment[
-                "npm_config_confluent_kafka_javascript_binary_host_mirror"
+                "npm_config_confluent_kafka-javascript_binary_host_mirror"
             ],
             mirror,
         )
